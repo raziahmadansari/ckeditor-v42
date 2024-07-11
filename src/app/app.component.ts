83a5d7749d0caabf177b51e1fab4fe5b85ff578e
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2024-07-11 11:39:18 +0530

    added dynamic editor rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { EditorComponent } from './editor/editor.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ckeditor-v42';
+
+  @ViewChild('viewContainer', { static: true, read: ViewContainerRef })
+  viewContainer!: ViewContainerRef;
+
+  editorCount: number = 200;
+  editors: Array<number> = [];
+  isLayoutReady: boolean = false;
+
+  constructor(private readonly changeDetector: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    // for (let i = 1; i <= this.editorCount; i++) {
+    //   this.editors.push(i);
+    // }
+
+    // this.isLayoutReady = true;
+    // this.changeDetector.detectChanges();
+
+    this.renderEditors();
+  }
+
+  renderEditors(): void {
+    this.viewContainer.clear();
+    for (let i = 1; i <= this.editorCount; i++) {
+      this.viewContainer.createComponent(EditorComponent);
+    }
+
+    this.changeDetector.detectChanges();
+  }
 }
